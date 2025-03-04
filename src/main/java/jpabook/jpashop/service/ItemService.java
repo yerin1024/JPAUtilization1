3dd5360b1aeb6d9843df0a63a53e5c767340c77b
yerin1024 <yerin102@nate.com>
2025-03-04 16:31:33 +0900

    상품 주문/ 주문 목록검색, 취소 개발(33~35강)

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,15 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    //변경 감지 기능을 사용하기 위해
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId); //다시한번 엔티티를 조회해서 영속 엔티티를 만들고 dirty checking 사용
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public Item findOne(Long itemId) {
